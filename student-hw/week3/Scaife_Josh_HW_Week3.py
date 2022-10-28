acfd8cd42931475a94e63fb29a32e2608d86d295
JoshuaScaife <joshuamscaife@gmail.com>
2022-10-28 12:48:35 -0500

    Adding Homework

--- student-hw/week3/Scaife_Josh_HW_Week3.py
+++ student-hw/week3/Scaife_Josh_HW_Week3.py
@@ -1,20 +1,48 @@
 
 
-
-
-Facebook_Username=(input ("Username: "))
-Facebook_Password =(input ("Password: "))
+# Facebook_Username=(input ("Username: ")) #this is where it prompts username/password
+# Facebook_Password =(input ("Password: "))
 Saved_Username= ("JoshuaScaife")
 Saved_Password = ("mine")
+running = True
 
-if Facebook_Username == Saved_Username :print(" ")
-else: print('Unknown User')
+while running:
+    Facebook_Username=(input ("Username: ")) #this is where it prompts username/password
+    if Facebook_Username == Saved_Username :
+        Facebook_Password = (input ("Password: "))
+        if (Facebook_Password) == (Saved_Password) :
+            print ("Is this you? "), print (Facebook_Username)
+            YesOrNo = input("yes or no: ")
+            if YesOrNo == "yes" :
+                print (("Welcome to Facebook"),(Facebook_Username))
+                running= False
+            elif YesOrNo == "no":
+                running = False
+            else: print ("Try again")
+    else: print ("Unknown User")
+    
+    
+    
 
-if (Facebook_Password) == (Saved_Password) :print ("Is this you? "), print (Facebook_Username)
-else: print ("Incorrect password", Facebook_Password), print ("Please re-enter username and password")
+# number = 23
 
 
-if (input ("yes or no: ")) == "yes" :print (("Welcome to Facebook"),(Facebook_Username))
-else: print ("Please re-enter username and password")
+# while running:
+#     guess = int(input('Enter an integer : '))
+
+#     if guess == number:
+#         print('Congratulations, you guessed it.')
+#         # this causes the while loop to stop
+#         running = False
+#     elif guess < number:
+#         print('No, it is a little higher than that.')
+#     else:
+#         print('No, it is a little lower than that.')
+# else:
+#     print('The while loop is over.')
+#     # Do anything else you want to do here
+
+# print('Done')
 
 
+# # comment out = ctr + /
---
+++ student-hw/week4/Scaife_Joshua_HW_Week6.py
@@ -0,0 +1,39 @@
+# The 'else' statement is used as a backup for if the inital intented output does not occur.
+# The 'elif' statement is used in between if and else statements to add extra possible oucomes if needed.
+# The 'while' statement creates a looping comand.
+
+# number = 22
+
+# if number > 22:
+#     print("A")
+# elif number == 23:
+#     print("B")
+# elif number >= 22:
+#     print("C")
+# C is printed.
+
+# loops = 20
+# number_loops = 1
+# while loops != 0:
+#     if loops % 2 == 0:
+#         loops = loops - 2
+#     print (number_loops)
+#     number_loops += 1
+# It will repeat 10 times.
+# While repeats the 'if' command until certain criteria are met.
+
+# for i in range(1, 5):
+#     print(i)
+# else:
+#     print('The for loop is over')
+# The output is the numbers 1-4 and "the for loop is over".
+# The 'for' keyword sets the variable i to a string in the range 1-5.
+# Range is used to establish a set of numbers that the 'for' command can check.
+# Break is used to stop a looping comand and exit.
+# Continue is used to continue a looping comand after a function is completed.
+# 11
+
+# for i in range(1, 100):
+#     print(i)
+#     if i > 10:
+#         break
